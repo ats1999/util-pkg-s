733de4969ea770f731166c573befea7eb1184fa5
Rahul kumar <rkt82224@gmail.com>
2021-03-22 22:21:04 +1100

    asd

--- bdevg/src/ev/util/toggleEditorFullScreen.js
+++
@@ -1,7 +0,0 @@
-const toggleEditorFullScreen = (el) =>{
-    if(el.style.height != "100vh")
-      el.style = "height:100vh; width:100vw; position:fixed;z-index:100000;top:0px;left:0px;background-color:white;";
-    else el.style = "height:400px;"
-}
-
-module.exports = toggleEditorFullScreen;
