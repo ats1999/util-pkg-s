79b2d5e421fd3859eccf9dffa8fb3aeb74c0ffa5
Rahul kumar <rkt82224@gmail.com>
2021-03-28 15:03:35 +1100

    asd

---
+++ ev/example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { EDEV,VDEV,HDEV } from '@bdevg/ev';
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+const App = () => {
+  return <EDEV getMd={(md)=>console.log()} 
+    getTitle={(title)=>console.log(title)}
+  />
+}
+
+export default App
